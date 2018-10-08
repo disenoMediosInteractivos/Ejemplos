8b43a495d3b80c3bedbf84b60daed412d394c43c
laurajunco <lc.junco10@uniandes.edu.co>
2018-10-07 20:03:54 -0500

    Acabar ejemplo movimiento simple

--- 05_gestos_mobile/d_movimiento_simple/sketch.js
+++ 05_gestos_mobile/d_movimiento_simple/sketch.js
@@ -4,21 +4,29 @@ var tam; //variable para tamano de la elipse
 function setup() {
   //crea un canvas del tamano de la ventana
   createCanvas(windowWidth, windowHeight);
+  background(0);
 
   //inicializa las variables de posicion y tamano
   x = width/2;
   y = height/2;
-  tam = 30;
+  tam = 60;
 
 }
 
 function draw() {
-  background(0);
+  //efecto de motion blur
+  fill(0, 5);
+  rect(-3, -3, width+6, height+6);
+
+  //texto
+  fill(255);
+  noStroke();
+  text("Mueva la elipse con el dedo", 20, 30);
 
   //pinta la elipse en la posicion x, y
   fill(255, 0, 0);
   stroke(255);
-  strokeWeight(2);
+  strokeWeight(3);
   ellipse(x, y, tam, tam);
 
 }
@@ -28,11 +36,11 @@ function touchMoved() {
 
     //revisa si se estaba tocando la bola
     //se usa touches[0] porque se asume que solo hay un toque a la vez
-    if (dist(touches[0].x, touches[0].y, x, y) < 15) {
+    if (dist(touches[0].x, touches[0].y, x, y) < tam/2) {
 
       //actualiza la posicion de la elipse con la posicion del toque
       x = touches[0].x;
       y = touches[0].y;
     }
-  return false;
+  return false; //elimina el comportamiento estandar del navegador al arrastrar un toque
 }

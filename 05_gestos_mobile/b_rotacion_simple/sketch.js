61b9281be6fb51b1046a64c56f8456d7005b41f1
laurajunco <lc.junco10@uniandes.edu.co>
2018-10-07 19:54:55 -0500

    Acabar ejemplo rotacion simple

--- 05_gestos_mobile/b_rotacion_simple/sketch.js
+++ 05_gestos_mobile/b_rotacion_simple/sketch.js
@@ -1,5 +1,6 @@
 var x, y; //variable para la posicion de la bola en x y en y;
 var velx, vely; //variables para velocidad o cambio de posicion en el tiempo
+var tam = 30; //variable tamaño de la elipse
 
 
 function setup() {
@@ -23,26 +24,26 @@ function draw() {
   //imprime los valores de rotacion en X y en Y
   fill(255);
   noStroke();
-  text("X: " + floor(rotationY), 20, 40);
-  text("Y: " + floor(rotationX), 20, 20);
+  text("rotación en X: " + floor(rotationY), 20, 40);
+  text("rotación en Y: " + floor(rotationX), 20, 20);
   text("vel X: " + velx, 20, 60);
   text("vel Y: " + vely, 20, 80);
 
   //asigna valores a la velocidad de acuerdo a la rotación del dispositivo
-  velx = map(rotationX, -90, 90, -3, 3);
-  vely = map(rotationY, -90, 50, -3, 3);
+  velx = map(rotationY, -90, 90, -2, 2);
+  vely = map(rotationX, -90, 90, -2, 2);
 
   //pinta la elipse
   fill(255, 0, 0);
   strokeWeight(2);
   stroke(255);
-  ellipse(x, y, 30, 30);
+  ellipse(x, y, tam, tam);
 
   //suma la velocidad en x y en y a las posiciones de la elipse
   x = x + velx;
   y = y + vely;
 
   //evita que las posiciones se salgan del canvas
-  x = constrain(x, 0, width);
-  y = constrain(y, 0, height);
+  x = constrain(x, tam/2, width - tam/2);
+  y = constrain(y, tam/2, height - tam/2);
 }

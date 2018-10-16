ebda460f71c6acae3110d5165885e32de4f8ce24
laurajunco <lalaj31@hotmail.com>
2018-10-16 12:23:24 -0500

    Update geolocation_mappa example

--- 06_geolocation/03_map/sketch.js
+++ 06_geolocation/03_map/sketch.js
@@ -8,51 +8,46 @@ let canvas;
 const mappa = new Mappa('Leaflet');
 
 var options = {
-  lat: 	0,
-  lng: 	0,
-  zoom: 14,
+  lat: 0,
+  lng: 0,
+  zoom: 8,
   style: "https://{s}.tile.osm.org/{z}/{x}/{y}.png"
 }
 
-
-//ideally I would fill options with locationData in a preload function
-
 function setup() {
-  
-  canvas = createCanvas(windowWidth, windowHeight);
 
-   if (geoCheck() == true) {
-     	getCurrentPosition(doThisOnLocation);
-   }
+  canvas = createCanvas(500, 500);
+
+  console.log('geoCheck: ' + geoCheck())
+  getCurrentPosition(doThisOnLocation);
+
 }
 
-function drawMap(){
-  myMap = mappa.tileMap(options); 
+function doThisOnLocation(position) {
+
+  options.lat = position.latitude
+  options.lng = position.longitude
+  lat = position.latitude
+  lng = position.longitude
+
+  if (!myMap)
+  drawMap();
+
+}
+
+function drawMap() {
+  myMap = mappa.tileMap(options);
   myMap.overlay(canvas);
   myMap.onChange(drawPoint);
-  console.log(locationData);
-}
 
+}
 
 function drawPoint() {
 
   clear();
-  
-  const pos = myMap.latLngToPixel(lat, lng); 
-  
-  fill(200, 100, 100);
-  ellipse( pos.x,  pos.y, 20, 20);
-  
+  var pos = myMap.latLngToPixel(lat, lng);
+
+  fill(0, 255, 0);
+  ellipse(pos.x, pos.y, 20, 20);
+
 }
-
-function doThisOnLocation(locationData){
-  
-  	options.lat = locationData.latitude
-  	options.lng = locationData.longitude
-  	lat = locationData.latitude
-  	lng = locationData.longitude
-  
-  	drawMap();
-}
-
-

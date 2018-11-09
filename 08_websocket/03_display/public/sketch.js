cd536296279e59e88baae5055527eeb7b303d077
laurajunco <lc.junco10@uniandes.edu.co>
2018-11-09 12:39:17 -0500

    Keep working on example 3

--- 08_websocket/03_display/public/sketch.js
+++ 08_websocket/03_display/public/sketch.js
@@ -1,53 +1,106 @@
 var socket;
 var display = false;
+var tam;
+var players = [];
 var buttons = [];
-var ip = "http://157.253.148.78:3000";
-
-
+var ip = "0.0.0.0:3000"
+//var ip = "http://157.253.148.78:3000";
 
 var upbtn, downbtn, leftbtn, rightbtn;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   noStroke();
-  var tam = Math.min(width, height)/5;
+  tam = Math.min(width, height)/5;
 
   socket = io.connect(ip);
-  console.log('ip', ip);
-
   socket.emit('start');
-
   socket.on('display', function(data) {
     display = data;
   });
 
-  upbtn = new Button(width/2, height/2 - tam, 'UP');
-  downbtn = new Button(width/2, height/2 + tam, 'DOWN');
-  leftbtn = new Button(width/2 - tam, height/2, 'LEFT');
-  rightbtn = new Button(width/2 + tam, height/2, 'RIGHT');
+  if(!display) {
 
-  buttons.push(upbtn, downbtn, leftbtn, rightbtn);
+    upbtn = new Button(width/2, height/2 - tam, 'UP');
+    downbtn = new Button(width/2, height/2 + tam, 'DOWN');
+    leftbtn = new Button(width/2 - tam, height/2, 'LEFT');
+    rightbtn = new Button(width/2 + tam, height/2, 'RIGHT');
+
+    buttons.push(upbtn, downbtn, leftbtn, rightbtn);
+  }
+
+  socket.on('heartbeat', function(data) {
+    players = data;
+
+    for (var i = 0; i < players.length; i++) {
+      players[i] = new Player(players[i].x, players[i].y, players[i].velX, players[i].velY)
+    }
+
+  });
 }
 
 function draw() {
-  if (display){
-    background(255, 0, 0);
-    //dibujar comida y serpientes
-    
+
+  if (display) {
+
+    background(0);
+
+    for( var i = 0; i < players.length; i++) {
+
+      players[i].mostrar();
+      players[i].mover();
+
+      data = {
+        x: players[i].x,
+        y: players[i].y,
+        velX: players[i].velX,
+        velY: players[i].velY
+      };
+
+      socket.emit('update', data);
+    }
   } else {
+
     background(255, 255, 0);
 
-    //dibujar controles
     for ( var i = 0; i < buttons.length; i++) {
+
       buttons[i].mostrar();
     }
-
   }
 }
 
 function mousePressed() {
   for ( var i = 0; i < buttons.length; i++) {
     buttons[i].oprimir(mouseX, mouseY);
+
+    if(buttons[i].oprimido){
+
+      data = buttons[i].dir;
+      socket.emit('dir', data);
+
+    }
+  }
+}
+
+function Player(x, y, velx, vely) {
+  this.x = x;
+  this.y = y;
+  this.tam = 20;
+  this.velX = velx;
+  this.velY = vely;
+
+  this.mostrar = function() {
+    fill(0, 255, 0);
+    rect(this.x, this.y, this.tam, this.tam);
+  }
+
+  this.mover = function() {
+    this.x += this.velX * 2;
+    this.y += this.velY * 2;
+
+    this.x = constrain(this.x, this.tam, width - this.tam);
+    this.y = constrain(this.y, this.tam, height - this.tam);
   }
 }
 
@@ -57,8 +110,10 @@ function Button(x, y, dir){
   this.dir = dir;
   this.tam = Math.min(width, height)/5;
   this.col = color(255);
+  this.oprimido = false;
 
   this.mostrar = function() {
+
     fill(this.col);
     stroke(255, 0, 0);
     strokeWeight(4);
@@ -85,10 +140,11 @@ function Button(x, y, dir){
     if( x > this.x - this.tam/2 && x < this.x + this.tam/2 &&
     y > this.y - this.tam/2 && y < this.y + this.tam/2) {
       this.col = color(0, 0, 255);
-
+      this.oprimido = true;
     } else {
 
       this.col = color(255);
+      this.oprimido = false;
     }
 
   }
--- 08_websocket/03_display/server.js
+++ 08_websocket/03_display/server.js
@@ -11,7 +11,7 @@ var io = socket(server);
 var display;
 var players = [];
 
-setInterval(heartbeat, 33);
+setInterval(heartbeat, 2000);
 
 function heartbeat() {
  io.sockets.emit('heartbeat', players);
@@ -24,10 +24,10 @@ function Display(id) {
 function Player(x, y, id) {
   this.x = x;
   this.y = y;
-  this.id = id
-  this.tam = 0;
+  this.id = id;
+  this.velX = 1;
+  this.velY = 0;
 }
-
 
 io.sockets.on('connection', newConnection)
 
@@ -35,9 +35,9 @@ function newConnection(socket) {
 
   console.log('new connection ');
 
-
   socket.on('start', start);
   socket.on('update', update);
+  socket.on('dir', dir);
   socket.on('disconnecting', disconnect);
 
   //start
@@ -46,7 +46,6 @@ function newConnection(socket) {
     if (display == undefined) {
 
       display = new Display(socket.id);
-
       io.to(socket.id).emit('display', true);
       console.log ('display is set up');
 
@@ -54,9 +53,7 @@ function newConnection(socket) {
 
     if (display.id !== socket.id) {
 
-      console.log ('first client');
-
-      player = new Player(0, 0, socket.id);
+      player = new Player(50, 50, socket.id);
       players.push(player);
       console.log(players.length + ' players');
 
@@ -68,10 +65,42 @@ function newConnection(socket) {
 
     for (var i = 0; i < players.length; i++) {
 
+
       if(players[i] != undefined && socket.id == players[i].id) {
 
         players[i].x = data.x;
         players[i].y = data.y;
+        players[i].velX = data.velX;
+        players[i].velY = data.velY;
+
+      }
+    }
+  }
+
+  //change direciton
+  function dir(data) {
+
+    for (var i = 0; i < players.length; i++) {
+
+      if(players[i] != undefined && socket.id == players[i].id) {
+
+        if(data === "UP") {
+          players[i].velX = 0;
+          players[i].velY = -1;
+
+        } else if (data === "DOWN"){
+          players[i].velX = 0;
+          players[i].velY = 1;
+
+        } else if (data === "LEFT"){
+          players[i].velX = -1;
+          players[i].velY = 0;
+
+        } else if (data === "RIGHT"){
+          players[i].velX = 1;
+          players[i].velY = 0;
+
+        }
       }
     }
   }

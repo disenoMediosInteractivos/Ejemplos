0a42632fd520a9693a68e235038d38c2696de8e6
laurajunco <lc.junco10@uniandes.edu.co>
2018-11-10 22:32:19 -0500

    Comentar ejemplo 1

--- 08_websocket/01_connect/public/sketch.js
+++ 08_websocket/01_connect/public/sketch.js
@@ -1,31 +1,43 @@
-var socket;
-var ip = "http://157.253.148.78:3000"; //change ip
+var socket; //variable para el socket
+
+//dirección ip del dispositivo
+var ip = "http://0.0.0.0:3000"; //reemplazar!
 
 
 function setup() {
+
   createCanvas(windowWidth, windowHeight);
   background(255);
   noStroke();
 
+  //conectar el socket al servidor de la ip del dispositivo
   socket = io.connect(ip);
+
+  //cuando se reciba el mensaje mouse, se ejecuta la funcion newDrawing
   socket.on('mouse', newDrawing);
 }
-
+//esta función se ejecuta cada vez que el mouse se arrastra por la pantalla
 function mouseDragged() {
+
+  //en la variable data se guarda la posición del mouse
   var data = {
     x: mouseX,
     y: mouseY
   }
 
-  //envia la información del mouse
+  //envia la información del mouse al servidor
   socket.emit('mouse', data);
 
+  //dibuja una elipse en la posición del mouse
   fill(255, 0, 0);
   ellipse(mouseX, mouseY, 15, 15);
 }
 
-//recibe información del servidor
-function newDrawing(data){
+//se ejecuta cuando recibe el mensaje 'mouse del servidor'
+function newDrawing( data ) { //data son los datos que envia el servidor al socket
+
   fill(0, 0, 255);
+
+  //dibuja una ellipse en las coordenadas recibidas
   ellipse(data.x, data.y, 15, 15)
 }
--- 08_websocket/01_connect/server.js
+++ 08_websocket/01_connect/server.js
@@ -1,22 +1,29 @@
 var express = require('express');
-var app = express();
-var server = app.listen(3000);
+var app = express(); //crea una app de express
+var server = app.listen(3000); //inicia un servidor en el puerto 3000
 
+ //muestra en la pagina los contenidos de la carpeta 'public'
 app.use(express.static('public'));
 
 console.log("socket server is running");
 
-var socket = require('socket.io');
-var io = socket(server);
 
+var socket = require('socket.io'); //importa la libreria express
+var io = socket(server); //crea un servidor de socket.io
+
+//cada vez que el servidor recibe una nueva conexión llama a la función newConnection
 io.sockets.on('connection', newConnection)
 
-function newConnection(socket) {
+
+function newConnection(socket) { //'socket' es la información del cliente que se conecto
   console.log('new connection ' + socket.id);
+
+  //cuando recibe el mensaje 'mouse' ejecuta la función mouseMessage
   socket.on('mouse', mouseMessage);
 
-  function mouseMessage(data) {
-    //emits message to all other sockets
+  function mouseMessage(data) { //recibe lso datos del mouse del cliente
+
+    //reenvia el mensaje con lso datos a todos los clientes excepto a quien envió el mensaje
     socket.broadcast.emit('mouse', data);
   }
 }
